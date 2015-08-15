d3b8b45490b213cb15df95bc9d541e6e734dc5fd
Edmond Zhang <edmond.zlg@gmail.com>
2015-08-16 07:07:33 +0800

    静态变量

---
+++ src/main/java/org/edmond/jimi/Constants.java
@@ -0,0 +1,10 @@
+package org.edmond.jimi;
+
+public class Constants {
+	public static final String GLOBLE_RESULT_SUCCESS="success";
+	public static final String GLOBLE_RESULT_DATA="data";
+	
+	public static final String STATUS_ENABLED="enabled";
+	public static final String STATUS_DISABLED="disabled";
+	public static final String STATUS_DELETED="deleted";
+}
